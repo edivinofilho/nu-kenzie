0a59e69139aa5f0f8d12e556fc9fd0d74ffb2fbe
edivinofilho <edivinofilho@hotmail.com>
2023-06-01 16:43:42 -0300

    fix: correção do link input no componente Form

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,13 @@
 import { StyleHeader } from "./style"
 import { StyledMainContainer } from "../../styles/Container"
 
+import Logo from "../../assets/logo.svg"
+
 export const Header = () => {
   return (
     <StyleHeader>
       <StyledMainContainer>
-        <img src="../../src/assets/logo.svg" alt="Logo Nu Kenzie nas cores rosa e preto" />
+        <img src={Logo} alt="Logo Nu Kenzie nas cores rosa e preto" />
       </StyledMainContainer>
     </StyleHeader>
   )

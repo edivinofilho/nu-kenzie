c10459d4f7498132f66d6ec350472863b0b07cf6
edivinofilho <edivinofilho@hotmail.com>
2023-06-02 13:06:49 -0300

    fix: padronização do formato dos imports

--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled from "styled-components"
 
 export const StyleHeader = styled.header`
   width: 100%;
--- src/styles/Container.js
+++ src/styles/Container.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled from "styled-components"
 
 export const StyledMainContainer = styled.main`
   width: 100%;
